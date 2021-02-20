2741b14815dbc4ad6c034832491b0b491993a2ec
jarryd smyth <jarryd@myconnect.com.au>
2021-02-20 15:03:07 +1100

    Create edit/update methods and views

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,24 +1,40 @@
 class ToursController < ApplicationController
     # skip_before_action :authenticate_user!, only: [:index, :show]
-    # before_action :find_tour, only: [:edit, :show, :update, :destroy]
-    
+    before_action :find_tour, only: [:edit, :show, :update, :destroy]
+
   def new
       @tour = Tour.new
   end
 
   def create
       @tour = Tour.new(tour_params)
-      @tour.user = current_user
+      @tour.user_id = current_user.id
       @tour.save
       redirect_to tour_path(@tour)
   end
     
   def show
-    @tour = Tour.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    @tour.update(tour_params)
+    redirect_to tour_path(@tour)
+  end
+
   def destroy
-    @tour = Tour.find(params[:id])
     @tour.destroy
     redirect_to tours_path
+  end
+
+  private
+  def find_tour
+    @tour = Tour.find(params[:id])
+  end
+
+  def tour_params
+    params.require(:tour).permit(:name, :description)
   end
 end
