eb00816409bf7fbcb8627c23c71d6bf1fa1c7460
Vanessa Klevenz <vanessaklevenz@gmail.com>
2021-02-20 15:11:54 +1100

    edit for activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,18 @@
 class ActivitiesController < ApplicationController
+  before_action :find_activity, only: [:edit, :show, :update, :destroy]
+
   def new
   end
 
   def edit
+  end
+
+  def update
+    @activity.update(activity_params)
+    redirect_to activities_path
+  end
+
+  def find_activity
+    @activity = Activity.find(params[:id])
   end
 end
--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -16,6 +16,7 @@ class ToursController < ApplicationController
   def show
     @tour = Tour.find(params[:id])
   end
+  
   def destroy
     @tour = Tour.find(params[:id])
     @tour.destroy
