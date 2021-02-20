dc1d6f3e1f7d79a80dc80c97fbfbb90ca1005345
Vanessa Klevenz <vanessaklevenz@gmail.com>
2021-02-20 13:13:33 +1100

    create and new tour controller

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,2 +1,16 @@
 class ToursController < ApplicationController
+    # skip_before_action :authenticate_user!, only: [:index, :show]
+    # before_action :find_tour, only: [:edit, :show, :update, :destroy]
+    
+    def new
+        @tour = Tour.new
+    end
+    
+    def create
+        @tour = Tour.new(tour_params)
+        @tour.user = current_user
+        @tour.save
+        redirect_to tour_path(@tour)
+    end
+    
 end
