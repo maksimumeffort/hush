10a72b13f2810ea884c03205d8df32b52d7402ae
Vanessa Klevenz <vanessaklevenz@gmail.com>
2021-02-20 12:50:33 +1100

    stashed

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,6 @@
 class Tour < ApplicationRecord
     has_many :tours, :tour_activities, :bookings
-    # has_many :activities, through: :tour_activities
+    has_many :activities, through: :tour_activities
     belongs_to :tour, :booking
     validates :name, :description, presence: true
 end
