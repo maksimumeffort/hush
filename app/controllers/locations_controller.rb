321acf93ef8ddd407748bf0a753cababa4454f29
jarryd smyth <jarryd@myconnect.com.au>
2021-02-20 16:17:13 +1100

    Add Edit Method

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,24 @@
 class LocationsController < ApplicationController
+  before_action :find_location, only: [:show, :edit, :update, :destroy]
+
   def new
   end
 
   def edit
+  end
+
+  def update
+    @location.update(location_params)
+    redirect_to location_path(@location)
+  end
+
+  private 
+
+  def find_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:latitude, :longitude, :address, :opening_time, :closing_time)
   end
 end
