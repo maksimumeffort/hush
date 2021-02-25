a0e67515236cbd093556119b3bb36622b8351e71
jarryd smyth <jarryd@myconnect.com.au>
2021-02-25 19:53:14 +1100

    Make publish method

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :tours
+  resources :tours do
+    member do
+      patch :publish
+    end
+  end
   resources :activities, only: [:new, :create, :edit]
   resources :locations, only: [:new, :create, :edit, :update, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
