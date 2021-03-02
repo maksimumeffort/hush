7f21d08962a6d5aee617bca63b5eed5ef6b7523c
Alex M <alexmaksimets@gmail.com>
2021-03-02 20:34:30 +1100

    added .env

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,8 @@
 class Location < ApplicationRecord
     has_many :activities
     belongs_to :user
-
+    geocoded_by :address
+    after_validation :geocode, if: :will_save_change_to_address?
     validates :longitude, :latitude, :address, presence: true
 
 end
