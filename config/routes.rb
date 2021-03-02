4050dd134fe973da1fa695350ef98dbee657fd54
Alex M <alexmaksimets@gmail.com>
2021-03-02 19:56:50 +1100

    updated schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- 
+
   get 'tour_activity/edit'
 
   devise_for :users
@@ -19,4 +19,3 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
- 
