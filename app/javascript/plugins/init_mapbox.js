2ef245b9cae2fec9948a9c64b950fc6eabec23ca
Alex M <alexmaksimets@gmail.com>
2021-03-18 19:28:34 +1100

    directions finally working, still need to style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,6 @@
-//Guido_js
 
+
+// Guido Original
 import mapboxgl from "mapbox-gl";
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
@@ -12,49 +13,79 @@ const flyOnMap = (map, card) => {
     zoom: 15,
   });
 };
+
+
 const initMapbox = () => {
   const mapElement = document.getElementById("map");
   if (mapElement) {
     // only build a map if there's a div#map to inject into
+
+    // mapbox element
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const geoJsonFeatures = []
+    // write a little function that creates an array in the format that we need with the data from the markers
+    const createFeaturesForGeoJsonObject = (markers) => {
+      // console.log(markers)
+      markers.forEach((marker) => {
+        geoJsonFeatures.push(
+          {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [marker.lng, marker.lat]
+            }
+            // 'properties': {
+            //   'title': 'Address',
+            //   // TODO: get address on the markers somehow
+            //   'description': `${marker.address}`
+            // }
+          }
+        )
+      });
+    };
+    // get markers
+    const markers = JSON.parse(mapElement.dataset.markers);
+    createFeaturesForGeoJsonObject(markers)
+      // console.log(geoJsonFeatures)
+
+    // assign our new features array to our geojson object
+    const geojson = {
+      'type': 'FeatureCollection',
+      'features': geoJsonFeatures
+    };
     const map = new mapboxgl.Map({
       container: "map",
-      style: "mapbox://styles/alemaks1993/ckm7pyk1ma1ap17qs70bur639",
-      options: {controls: {profileSwitcher: false}, profile: "walking"},
-      properties: {
-        'marker-color': '#8FBF5F',
-        'marker-size': 'large',
-        'marker-symbol': 'rocket'},
+      style: "mapbox://styles/alemaks1993/ckm7pyk1ma1ap17qs70bur639"
+
     });
     const directions = new MapboxDirections({
       accessToken: mapElement.dataset.mapboxApiKey,
       unit: 'metric',
       profile: 'mapbox/walking',
-      controls: { profileSwitcher: false, instructions: false, inputs: false },
-      style: [{
-        'id': 'directions-waypoint-point-casing',
-        'type': 'circle',
-        'source': 'directions',
-        'paint': {
-        'circle-radius': 8,
-        'circle-color': '#8FBF5F'
-        }
-      }]
-    });
-    // directions conditional (only run if page = show)
-      if (window.location.pathname == "show") {
-      map.addControl(directions)};
-      const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach((marker) => {
-        new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
-      });
-      const fitMapToMarkers = (map, markers) => {
-        const bounds = new mapboxgl.LngLatBounds();
-        markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
-        map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+      controls: { profileSwitcher: false, instructions: false, inputs: false }
 
+    });
+
+
+    // directions conditional (only run if page = show)
+    //   if (window.location.href.includes('tours')) {
+    //     map.addControl(directions)
+    // };
+      geojson.features.forEach(function (marker) {
+      // create a HTML element for each feature
+      const el = document.createElement('div');
+      el.className = 'marker';
+      // make a marker for each feature and add it to the map
+      new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
+    });
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach((marker) => bounds.extend([marker.geometry.coordinates[0], marker.geometry.coordinates[1]]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
     };
-    fitMapToMarkers(map, markers);
+    fitMapToMarkers(map, geojson.features);
     const carousel = document.querySelector(".carousel");
     if (carousel) {
       $(".carousel").on("slid.bs.carousel", function () {
@@ -68,21 +99,57 @@ const initMapbox = () => {
         markers[markers.length - 1].lat,
       ];
       const middle = markers.slice(1, -1);
-      // var directions = new MapboxDirections({
-      //   accessToken: mapElement.dataset.mapboxApiKey,
-      //   unit: "metric",
-      //   profile: "driving",
-      //   interactive: false,
-        // container: "directions", // Specify an element thats not the map container.
-      // });
-      // map.addControl(directions);
+
       directions.setOrigin(start);
       middle.forEach((m, i) => directions.addWaypoint(i, [m.lng, m.lat]));
       directions.setDestination(end);
       directions.on("route", function (e) {
-        console.log(e.route); // Logs the current route shown in the interface.
-      });
+        console.log(e.route);
+     });
     }
+    if (window.location.href.includes('tours')) {
+        map.addControl(directions)
+        const createCoordinates = () => {
+        const coordinates = []
+      markers.forEach((marker) => {
+        coordinates.push(
+          [marker.lng, marker.lat]
+        )
+      });
+      return coordinates;
+    }
+  createCoordinates()
+  map.on('load', function () {
+        map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+        'type': 'LineString',
+        'coordinates': createCoordinates()
+        }
+        }
+        });
+        map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': '#888',
+        'line-width': 2
+        }
+        });
+    })
+    };
+
   }
+
+
+  ;
 };
 export { initMapbox };
